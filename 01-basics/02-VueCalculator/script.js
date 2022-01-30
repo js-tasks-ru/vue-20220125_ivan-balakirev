8f0fcc4ea510ae66bd462c34bdc6c29b59585cfa
Ivan Balakirev <ivan.balakirev@gmail.com>
2022-01-30 14:13:49 +0300

    2nd task Vue basics

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,33 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
 // Создайте Vue приложение
+const app = createApp( {
+  data() {
+    return {
+      result: 0,
+      first: 0,
+      last: 0,
+      method: 'sum',
+    }
+  },
+
+  computed: {
+    computedResult() {
+      const method = this.method;
+
+      if ( method === 'sum' ) {
+        this.result = this.first + this.last;
+      } else if ( method === 'subtract' ) {
+        this.result = this.first - this.last;
+      } else if ( method === 'multiply' ) {
+        this.result = this.first * this.last;
+      } else if ( method === 'divide' ) {
+        this.result = this.first / this.last;
+      }
+
+      return this.result;
+    }
+  },
+} );
+
+const vm = app.mount( '#app' );
--- 01-basics/04-MeetupTitle/script.js
+++ 01-basics/04-MeetupTitle/script.js
@@ -1,17 +1,41 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
-
-const API_URL = 'https://course-vue.javascript.ru/api';
-
-function fetchMeetupById(meetupId) {
-  return fetch(`${API_URL}/meetups/${meetupId}`).then((response) => {
-    if (response.ok) {
-      return response.json();
-    } else {
-      return response.json().then((error) => {
-        throw error;
-      });
-    }
-  });
-}
+import { createApp } from './vendor/vue.esm-browser.js';
 
 // Требуется создать Vue приложение
+const app = createApp( {
+  data() {
+    return {
+      title: '',
+      itemId: 1,
+    }
+  },
+  methods: {
+    fetchMeetupById( meetupId ) {
+      const API_URL = 'https://course-vue.javascript.ru/api';
+
+      return fetch( `${API_URL}/meetups/${meetupId}` ).then( ( response ) => {
+        if ( response.ok ) {
+          return response.json();
+        } else {
+          return response.json().then( ( error ) => {
+            throw error;
+          } );
+        }
+      } );
+    }
+  },
+  computed: {
+    meetup() {
+      this.fetchMeetupById( this.itemId ).then( ( meetup ) => {
+        this.title = meetup.title;
+      } );
+    }
+  },
+  watch: {
+    meetup: {
+      handler( newValue, oldValue ) {
+        this.title = oldValue;
+      }
+    },
+  },
+} );
+const vm = app.mount( '#app' );
