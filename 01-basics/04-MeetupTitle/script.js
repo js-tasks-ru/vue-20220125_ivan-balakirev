fbd752875d64e4152d78350bad4175fab8716dc9
Ivan Balakirev <ivan.balakirev@gmail.com>
2022-02-01 13:15:49 +0300

    CR fixes

--- 01-basics/04-MeetupTitle/script.js
+++ 01-basics/04-MeetupTitle/script.js
@@ -1,6 +1,19 @@
 import { createApp } from './vendor/vue.esm-browser.js';
 
-// Требуется создать Vue приложение
+function fetchMeetupById( meetupId ) {
+  const API_URL = 'https://course-vue.javascript.ru/api';
+
+  return fetch( `${API_URL}/meetups/${meetupId}` ).then( ( response ) => {
+    if ( response.ok ) {
+      return response.json();
+    } else {
+      return response.json().then( ( error ) => {
+        throw error;
+      } );
+    }
+  } );
+}
+
 const app = createApp( {
   data() {
     return {
@@ -8,32 +21,17 @@ const app = createApp( {
       itemId: 1,
     }
   },
-  methods: {
-    fetchMeetupById( meetupId ) {
-      const API_URL = 'https://course-vue.javascript.ru/api';
-
-      return fetch( `${API_URL}/meetups/${meetupId}` ).then( ( response ) => {
-        if ( response.ok ) {
-          return response.json();
-        } else {
-          return response.json().then( ( error ) => {
-            throw error;
-          } );
-        }
-      } );
-    }
-  },
-  computed: {
-    meetup() {
-      this.fetchMeetupById( this.itemId ).then( ( meetup ) => {
-        this.title = meetup.title;
-      } );
-    }
+  mounted() {
+    fetchMeetupById( this.itemId ).then( ( meetup ) => {
+      this.title = meetup.title;
+    } );
   },
   watch: {
-    meetup: {
+    itemId: {
       handler( newValue, oldValue ) {
-        this.title = oldValue;
+        fetchMeetupById( newValue ).then( ( meetup ) => {
+          this.title = meetup.title;
+        } );
       }
     },
   },
